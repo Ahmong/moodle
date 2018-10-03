e7ed5a61b71b7d55b2c96b615ce7de1c83fabb4d
Ahmong <ahmong@126.com>
2018-10-03 21:21:15 +0800

    feat: 添加本地修改、补充的简体中文语言包

---
+++ lang/zh_cn_local/completion.php
@@ -0,0 +1,38 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://47.94.85.212
+ *
+ * @package    core
+ * @subpackage completion
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activitieslabel'] = '活动 / 资源';
+$string['bulkactivitycompletion'] = '批量编辑活动跟踪状态';
+$string['bulkactivitydetail'] = '选择需要进行批量编辑的活动。';
+$string['defaultcompletion'] = '默认活动进度跟踪';
+$string['eventdefaultcompletionupdated'] = '用于活动跟踪更新的默认值';
+$string['manual'] = '手动';
+$string['privacy:metadata:coursemodulesummary'] = '保存用户的活动跟踪数据';
+$string['privacy:metadata:overrideby'] = '覆盖活动跟踪数据的用户的用户ID';
+$string['privacy:metadata:timemodified'] = '活动跟踪更改的时间';
+$string['privacy:metadata:userid'] = '课程和活动跟踪数据关联的用户的用户ID';
+$string['withconditions'] = '条件完成';
---
+++ lang/zh_cn_local/gradereport_user.php
@@ -0,0 +1,33 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://47.94.85.212
+ *
+ * @package    gradereport
+ * @subpackage user
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['eventgradereportviewed'] = '查看用户成绩报告';
+$string['myself'] = '我自己';
+$string['otheruser'] = '用户';
+$string['privacy:metadata:preference:gradereport_user_view_user'] = '在成绩报告中显示当前用户的成绩还是另一个用户的成绩';
+$string['tablesummary'] = '本表列出成绩项目并包括成绩项目的分类（如果成绩项目包含在某个分类下）。';
+$string['viewas'] = '报告内容';
